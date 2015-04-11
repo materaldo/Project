3b56dcc9307e67142f260d33d12bd8a18e2b143d
materaldo <mateusz19931122@gmail.com>
2015-04-11 22:01:09 +0200

    podstrona about

---
+++ Aplikacja/app/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('header')
+
+	<meta name="Description" content="" />
+	<meta name="Keywords" content="" />
+
+@stop
+
+@section('content')
+
+
+<p> O stronie </p>
+
+
+
+@stop
