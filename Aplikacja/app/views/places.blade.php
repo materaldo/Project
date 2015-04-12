291d570fc22284da6fa6a1d0ed90aa49cb1f14ba
joalic0393 <j.lichodij@gmail.com>
2015-04-12 11:03:42 +0200

    baza noclegowa 1.2 + przykładowe aktualności

--- Aplikacja/app/views/places.blade.php
+++ Aplikacja/app/views/places.blade.php
@@ -11,11 +11,13 @@
 @section('content')
 
 <h2>BAZA NOCLEGOWA</h2>
+<a class="clickMe" href="http://zpi.dev/index.php/add">Dodaj nowe miejsce</a>
+
 	<table id="baza">	
 		<tr>
 			<td></td>
 			<td>Nazwa, adres noclegu</td>
-			<td>Liczba miejsc zajętych</td>
+			<td>Liczba wolnych miejsc</td>
 			<td>Liczba miejsc ogółem</td>
 		</tr>	
 <?php
@@ -25,37 +27,18 @@
 		{
 			echo "<tr>
 				<td>
-					<img src=\"" . $noc->zdjecie . "\" alt=\"" . $noc->nazwa ."\"/>
+					<img src=\"" . $noc->image ."\" alt=\"" . $noc->nazwa ."\" height=\"150\" width=\"100\"/>
 				</td>
 				<td>" . 
 					$noc->nazwa . "<br>" . $noc->ulica . " " . $noc->nr_mieszkania . ", " . $noc->kod_pocztowy . " " . $noc->miejscowosc . 
 				"</td>
-				<td>" . 
+				<td align=\"middle\">" . 
 					$noc->miejsca_wolne . 
 				"</td>
-				<td>" . $noc->miejsca_ogolem . 
+				<td align=\"middle\">" . $noc->miejsca_ogolem . 
 			"</tr>";
 		}	
 ?>
-
-		
-				
-			<tr>
-				<td><img src="{{asset('images/hotel1.jpg')}}" alt="hotel1"/></td>
-				<td>Hotel Miejski<br>ul. Zielona 30a, 50-335 Wrocław</td>
-				<td align="middle">43</td>
-				<td align="middle">200</td>
-			</tr>
-			<tr>
-				<td><img src="{{asset('images/hotel2.jpg')}}" alt="hotel2"/></td>
-				<td>Szkoła Podstawowa im. Kazimierza Wielkiego<br>ul. Akademicka 21, 50-342 Wrocław</td>
-				<td align="middle">12</td>
-				<td align="middle">60</td>
-			</tr>
 </table>
-
-<p>Tutaj lista miejsc noclegowych pobrana z bazy danych</p>
-
-<a class="clickMe" href="http://zpi.dev/index.php/add">Dodaj nowe miejsce</a>
 
 @stop
