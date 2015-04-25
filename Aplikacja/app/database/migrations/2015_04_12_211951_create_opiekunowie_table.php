2e35ae356441057899c6a9b3caa701f99976fc4b
joalic0393 <j.lichodij@gmail.com>
2015-04-25 16:09:20 +0200

    baza 2.0

---
+++ Aplikacja/app/database/migrations/2015_04_12_211951_create_opiekunowie_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpiekunowieTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('protectors', function($table)
+		{
+			$table->integer('id')->unsigned();
+			$table->foreign('id')->references('id')->on('users');
+			$table->primary('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->date('date_of_birth');
+			$table->string('phone_number');
+			$table->string('alt_phone_number')->nullable();
+			$table->integer('id_coun')->unsigned();
+			$table->foreign('id_coun')->references('id')->on('countries');
+			$table->integer('id_first_lang')->unsigned();
+			$table->foreign('id_first_lang')->references('id')->on('languages');
+			$table->integer('id_second_lang')->unsigned()->nullable();
+			$table->foreign('id_second_lang')->references('id')->on('languages');
+			$table->integer('id_third_lang')->unsigned()->nullable();
+			$table->foreign('id_third_lang')->references('id')->on('languages');
+			$table->string('document_number');
+			$table->string('insurance_number');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('protectors');
+	}
+
+}
---
+++ Aplikacja/app/database/migrations/2015_04_12_212005_create_grupy_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGrupyTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		
+		Schema::create('groups', function($table)
+		{
+			$table->increments('id');
+			$table->integer('number_of_people');
+			$table->string('mean_of_transport')->nullable();
+			$table->integer('id_prot')->unsigned();
+			$table->foreign('id_prot')->references('id')->on('protectors');
+			$table->integer('id_coun')->unsigned();
+			$table->foreign('id_coun')->references('id')->on('countries');
+			$table->integer('id_first_lang')->unsigned();
+			$table->foreign('id_first_lang')->references('id')->on('languages');
+			$table->integer('id_second_lang')->unsigned()->nullable();
+			$table->foreign('id_second_lang')->references('id')->on('languages');
+			$table->integer('id_third_lang')->unsigned()->nullable();
+			$table->foreign('id_third_lang')->references('id')->on('languages');
+			$table->timestamps();
+		});
+		
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('groups');
+	}
+
+}
