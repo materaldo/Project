1d100e59dffc659c1b2cf2f07e9b029c6c74bd9c
MRUCZEK <askadra@wp.pl>
2015-05-18 16:00:21 +0200

    lang - default

--- Aplikacja/app/views/layouts/default.blade.php
+++ Aplikacja/app/views/layouts/default.blade.php
@@ -30,7 +30,7 @@ if (Auth::user()!==null)
             <div id="logo_text">
             </div>
             <div>
-				<nav id = "lang">Język                    <ul>
+				<nav id = "lang">{{Lang::get('default.lang')}}                   <ul>
                         <li>{{link_to_route('language.select', 'English', array('en'))}}</li>
                         <li>{{link_to_route('language.select', 'Polski', array('pl'))}} </li>
                     </ul>
