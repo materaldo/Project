6be5bde5549481156e8c7253ae38a21c4a0ea764
materaldo <mateusz19931122@gmail.com>
2015-05-18 16:24:15 +0200

    accomodation add

--- Aplikacja/app/database/seeds/OthersSeeder.php
+++ Aplikacja/app/database/seeds/OthersSeeder.php
@@ -14,8 +14,8 @@ class OthersSeeder extends Seeder
 		Participant::create(['id'=>'7','first_name'=>'Kamil','last_name'=>'Nowak','date_of_birth'=>'11.02.1990','phone_number'=>'377543427','id_gr'=>'1','id_coun'=>'1','id_first_lang'=>'2','document_number'=>'d82j83d9d','insurance_number'=>'jd29h3389j']);
 		Participant::create(['id'=>'8','first_name'=>'Joanna','last_name'=>'Nowakowska','date_of_birth'=>'11.02.1990','phone_number'=>'377543427','id_gr'=>'2','id_coun'=>'1','id_first_lang'=>'2','document_number'=>'d82j83d9d','insurance_number'=>'jd29h3389j']);
 		Participant::create(['id'=>'9','first_name'=>'Zbigniew','last_name'=>'B¹k','date_of_birth'=>'11.02.1990','phone_number'=>'377543427','id_gr'=>'2','id_coun'=>'1','id_first_lang'=>'2','document_number'=>'d82j83d9d','insurance_number'=>'jd29h3389j']);
+		Accommodation::create(['name'=>'Hotel Zacisze','street'=>'ul. zielona','building'=>'40','post_code'=>'54-322','city'=>'Wroc³aw','phone_number'=>'453453453','image'=>'http://ser1.hastalavista.pl/wp-content/uploads/2013/02/Hotel-Orbis1.jpg','map'=>'https://goo.gl/maps/lOZM2','free_places'=>'5','all_places'=>'5','price'=>'69']);
 		//Accommodation::create([]);
-		
 		
 
 
