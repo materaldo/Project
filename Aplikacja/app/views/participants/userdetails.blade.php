07bd1d7b8906162eae633244172686d9a5d81446
MBrozyna <matbrozyna7@gmail.com>
2015-06-01 12:09:24 +0200

    user details v2

--- Aplikacja/app/views/participants/userdetails.blade.php
+++ Aplikacja/app/views/participants/userdetails.blade.php
@@ -16,7 +16,18 @@
     $us=Participant::where('id', '=', $idU)->first();
 
        echo " Imię: ". $us->first_name . "<br><br> Nazwisko: " . $us->last_name . " <br><br> Data urodzenia: ". $us->date_of_birth. " <br><br> Numer telefonu: " .
-               $us->phone_number . "<br><br> Numer dokumentu: ". $us->document_number . "<br><br> Numer ubezpieczenia: " . $us->insurance_number 
+               $us->phone_number . "<br><br> Numer dokumentu: ". $us->document_number . "<br><br> Numer ubezpieczenia: " . $us->insurance_number ;
+        if($us->id_acc != NULL)
+            {
+                $accus = Accomodation::where('id', '=', $us->id_acc);
+                echo"<br><br> Nazwa zakwaterowania: " . $accus->name . " <br> Ulica: " . $accus-> street . " <br><br> Kod pocztowy: " . $accus-> post_code .
+                " <br><br> Miasto: " . $accus -> city . " <br><br> Numer telefonu: " . $accus-> phone_numer . " <br><br> Miejsce: " . $accus->image . " <br><br> Google Maps" . $accus->maps;
+            }
+        else
+            {
+                echo "<br><br>Zakwaterowanie: brak";
+            }
+
 ?>
 
 @stop
