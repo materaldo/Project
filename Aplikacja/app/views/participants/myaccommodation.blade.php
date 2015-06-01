b5ad2c6f4e6980df69505cbb30350c2429b41360
joalic0393 <j.lichodij@gmail.com>
2015-06-01 15:51:32 +0200

    Revert "Revert "..""
    This reverts commit c3f80ad42436885e6c1f0d6916d70066dbbbe6e3.

--- Aplikacja/app/views/participants/myaccommodation.blade.php
+++ Aplikacja/app/views/participants/myaccommodation.blade.php
@@ -17,11 +17,11 @@
 	$user = Auth::id();
 	
 	$participant = Participant::where('id', '=', $user)->first();
-	$accus = $participant->id_acco;
+	$accus = $Participant->id_acco;
 	
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=' , $accus)->first();
 	echo $ac->name;}
 	else{ echo  'Wkrótce...' ;
 		}
@@ -33,11 +33,11 @@
 	$user = Auth::id();
 	
 	$participant = Participant::where('id', '=', $user)->first();
-	$accus = $participant->id_acco;
+	$accus = $Participant->id_acco;
 	
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=', $accus)->first();
 	echo "ul. ";
 	echo $ac->street;}
 ?>
@@ -49,8 +49,8 @@
 	$accus = $participant->id_acco;
 	
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=', $accus)->first();
 	echo $ac->building;}
 ?>
 </p>
@@ -62,8 +62,8 @@
 	$accus = $participant->id_acco;
 	
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=', $accus)->first();
 	echo $ac->post_code;}
 ?> 
 <?php
@@ -73,20 +73,20 @@
 	$accus = $participant->id_acco;
 
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=', $accus)->first();
 	echo $ac->city;}
 ?>
 </p><p>
 <?php
-	$$user = Auth::id();
+	$user = Auth::id();
 	
 	$participant = Participant::where('id', '=', $user)->first();
 	$accus = $participant->id_acco;
 	
 	if($accus != null){
-	$idac = $accus->id;
-	$ac = Accommodation::where('id', '=', $idac)->first();
+	
+	$ac = Accommodation::where('id', '=', $accus)->first();
 	echo 'tel: '; 
 	echo $ac->phone_number;}
 	
