ee2374ee14544402d94eb0f1d2b3b7ba6c4dce08
MBrozyna <matbrozyna7@gmail.com>
2015-06-01 10:49:31 +0200

    poprawiono mail dodawania uzytkownika

--- Aplikacja/app/views/groups/message.blade.php
+++ Aplikacja/app/views/groups/message.blade.php
@@ -10,11 +10,14 @@
 
 @section('content')
 <form method="post" id="mailForm" action="{{URL::to('/group/messagesender')}}/{{$idG}}">
-        <label>Tytuł: </label><input name = "title" id="title" type = "text" size = "53" maxlength = "255"><br>
-
-    </form>
-	<textarea rows="4" cols="50" form="mailForm" name="emailText" id="emailText">
+        <input name = "title" id="title" type = "text" size = "53" maxlength = "255"value="tytuł"><br>
+<br>
+    <textarea rows="4" cols="50" form="mailForm" name="emailText" id="emailText">
             Treść maila
     </textarea>
+    <br><br><input type="submit" id="submit" value="Wyślij">
+    </form>
+<br>
+
 
 @stop
